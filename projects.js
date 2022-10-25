9c0509c2b812893ba3724e3719a3306b01b38039
Anna Axelsson <103879144+AnnaAxelsson051@users.noreply.github.com>
2022-10-25 23:30:12 +0200

    Added javascript functionality to menu

--- projects.js
+++ projects.js
@@ -1,3 +1,16 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+//since it returns an array we wanna get the first element which is the togglebutton
+
+const navLinks = document.getElementsByClassName("nav-links")[0];
+//select the main nav class
+
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+//add an eventlistener and say whenever this is clicked add the class active if its not already there and if there remove it
+
+//CAROUSEL CODE
+
 const buttons = document.querySelectorAll("[data-carousel-button]");
 //data-carousel-button is a data attribute that is selected here
 
